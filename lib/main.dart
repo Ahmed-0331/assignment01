9bbde10f521784aecee0c2f3fed261af2d544ded
ahmed joha <ahmedjoha650@gmail.com>
2025-12-28 15:27:56 +0600

    add

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+void main(){
+  Set<int>setA = {1,2,3,4,5};
+  Set<int>setB = {4,5,6,7,8};
+  print('Set A elements:');
+  for(var item in setA){
+    print(item);
+  }
+
+  print('Set B elements:');
+  for(var item in setB){
+    print(item);
+  }
+
+
+}
