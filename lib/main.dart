c87a3a874a9df1e597989d809064d5f22b8adee7
ahmed joha <ahmedjoha650@gmail.com>
2026-01-08 15:57:43 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 void main(){
-  Set<int>setA = {1,2,3,4,5};
-  Set<int>setB = {4,5,6,7,8};
-  print('Set A elements:');
-  for(var item in setA){
-    print(item);
+  double weight = 70.0;
+  double height = 1.73;
+  double bmi = BMICalculate( weight, height );
+  String category = getBMICategory(bmi);
+  print("Your BMI is:");
+  print(bmi);
+  print("Category:");
+  print(category);
+
+}
+
+String getBMICategory(double bmi){
+  if (bmi < 18.5 ){
+    return 'Underweight';
+  }else if (bmi >= 18.5 && bmi <= 24.9){
+    return 'Normal';
+  }else if ( bmi >= 25 && bmi <= 29.9){
+    return 'Overweight';
+  } else {
+    return 'Obese';
   }
-
-  print('Set B elements:');
-  for(var item in setB){
-    print(item);
-  }
-
-
+}
+double BMICalculate(double weight , double height){
+  double bmi = weight / (height * height);
+  return bmi;
 }
